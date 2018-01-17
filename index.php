9e8eb026fc8ae8fa394e9cf9793d436e42411afe
meganscarlett <35319845+meganscarlett@users.noreply.github.com>
2018-01-17 18:12:07 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,20 @@
 <?php
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
-    $from = 'From: apbiology'; 
-    $to = 'megan16scarlett@gmail.com'; 
-    $subject = 'Message';
-?>
-$body = "From: $name\n E-Mail: $email\n Message:\n $message";
-?>
+$name = $_POST[&#039name&#039];
+$email = $_POST[&#039email&#039];
+$message = $_POST[&#039message&#039];
+$from = &#039From: apbiology&#039; 
+$to = &#039megan16scarlett@gmail.com&#039; 
+$subject = &#039Hello&#039;
+$thankyou = &#039meganscarlett.github.io&#039;
 
-<?php
-if ($_POST['submit']) {
-    if (mail ($to, $subject, $body, $from)) { 
-        echo '<p>Your message has been sent!</p>';
-    } else { 
-        echo '<p>Something went wrong, go back and try again!</p>'; 
-    }
+$body = "From: $name\n E-Mail: $email\n Message:\n $message";
+
+if ($_POST[&#039submit&#039]) {
+if (mail ($to, $subject, $body, $from)) { 
+echo '';
+} else { 
+echo 'Something went wrong, go back and try again!'; 
+}
 }
 ?>
+<script>location.replace('') </script>
